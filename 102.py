6e10f664b53f89d4d69f6d5e5daf7f8c3ebded56
shellbye <Shellbye@users.noreply.github.com>
2020-07-08 17:26:42 +0800

    Create 102.py

---
+++ 102.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        ans = []
+        if not root:
+            return ans
+        s1 = [root]
+        s2 = []
+        while s1 or s2:
+            level = []
+            if s1:
+                while s1:
+                    # s1 为本轮出栈
+                    cur = s1[0]
+                    level.append(cur.val)
+                    if cur.left:
+                        s2.append(cur.left)
+                    if cur.right:
+                        s2.append(cur.right)
+                    s1 = s1[1:]
+            else:
+                while s2:
+                    # s2 为本轮出栈
+                    cur = s2[0]
+                    level.append(cur.val)
+                    if cur.left:
+                        s1.append(cur.left)
+                    if cur.right:
+                        s1.append(cur.right)
+                    s2 = s2[1:]
+            ans.append(level)
+        return ans
