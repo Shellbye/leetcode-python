cd81668def3a4e9d227b3123a9393144ad29dceb
shellbye <Shellbye@users.noreply.github.com>
2020-07-08 16:24:29 +0800

    Create 100.py

---
+++ 100.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None and q is None:
+            return True
+        elif p and q and p.val == q.val:
+            left = self.isSameTree(p.left, q.left)
+            right = self.isSameTree(p.right, q.right)
+            return left and right
+        else:
+            return False
