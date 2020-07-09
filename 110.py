21f4c124ef7710d59ab6933cd3ec84c06b8097fa
shellbye <Shellbye@users.noreply.github.com>
2020-07-09 19:14:21 +0800

    Update 110.py

--- 110.py
+++ 110.py
@@ -28,3 +28,42 @@ class Solution(object):
         if root.right:
             right_d = self.depth(root.right)
         return max(left_d, right_d) + 1
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class SolutionV2(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.balanceAndHeight(root)[0]
+    
+    def balanceAndHeight(self, root):
+        if not root:
+            return True, -1
+        leftBalance, leftHeight = False, 0
+        if root.left:
+            leftBalance, leftHeight = self.balanceAndHeight(root.left)
+        else:
+            leftBalance = True
+        if not leftBalance:
+            return False, leftHeight
+        
+        rightBalance, rightHeight = True, 0
+        if root.right:
+            rightBalance, rightHeight = self.balanceAndHeight(root.right)
+        else:
+            rightBalance = True
+        if not rightBalance:
+            return False, rightHeight
+        
+        diff = abs(leftHeight - rightHeight)        
+        return leftBalance and rightBalance and diff <= 1, max(leftHeight, rightHeight) + 1
