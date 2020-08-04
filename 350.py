072ee76d4c96e0c535c182b706d02ad4de5bc113
shellbye <Shellbye@users.noreply.github.com>
2020-08-04 23:37:58 +0800

    Update 350.py

--- 350.py
+++ 350.py
@@ -12,3 +12,27 @@ class Solution(object):
                     continue
                 ans.extend([v] * min(nums1.count(v), nums2.count(v)))
         return ans
+
+
+class SolutionV2(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+
+        ans = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                i = i + 1
+                j = j + 1
+            elif nums1[i] > nums2[j]:
+                j = j + 1
+            else:
+                i = i + 1
+        return ans   
