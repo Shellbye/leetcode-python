165b36776dbca4a5a19b134f7f874ff1a0b0989b
shellbye <Shellbye@users.noreply.github.com>
2020-10-08 21:30:23 +0800

    Create 563.py

---
+++ 563.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        t = self.doFindTilt(root)
+        t0 = self.findTilt(root.left)
+        t1 = self.findTilt(root.right)
+        return t + t0 + t1
+    
+    def doFindTilt(self, root):
+        if not root:
+            return 0
+        t0 = self.doCalcSum(root.left)
+        t1 = self.doCalcSum(root.right)
+        return abs(t0 - t1)
+    
+    def doCalcSum(self, root):
+        if not root:
+            return 0
+        t0 = self.doCalcSum(root.left)
+        t1 = self.doCalcSum(root.right)
+        return root.val + t0 + t1
