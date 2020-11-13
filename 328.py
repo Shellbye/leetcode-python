b077b76a8de9bb0d9718279cd6636c936972112b
shellbye <Shellbye@users.noreply.github.com>
2020-11-13 23:06:33 +0800

    Create 328.py

---
+++ 328.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+
+        even_head = head.next
+        even, odd = even_head, head
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
+        
